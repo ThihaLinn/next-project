1c8f5e1381f70f6271840e95b06ccb3be58ad002
Thiha Linn <linthiha964@gmail.com>
2024-05-18 19:35:06 +0630

    init next-dashboard

---
+++ nextjs-dashboard/app/ui/font.ts
@@ -0,0 +1,5 @@
+import { Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Roboto({ weight: '500', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '700', subsets: ['latin'] });
